0c417da3aa1c9d2a0919f364cfee1effccc9f65f
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-16 20:19:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(name){
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+  static sanitize(name){
+    return name.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+  static titleize(name){
+  let arrPro=[]
+  let nameArr=name.split(' ');
+  nameArr.map(word =>{
+   if(word !== 'the' && word !== 'a' && word !== 'an' && word !== 'but' && word !== 'of' && word !== 'and' && word !== 'for' && word !== 'at' && word !== 'by' && word !== 'from')
+   {
+     arrPro.push(word.charAt(0).toUpperCase() + word.slice(1));
+    
+   }
+   else{
+     arrPro.push(word);
+   }
+  
+  })
+   return arrPro.join(' ').charAt(0).toUpperCase() + arrPro.join(' ').slice(1);
+  }
 }
