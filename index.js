27190e62273dd53e3b4f3f52e3957dd9cff70e0b
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-16 20:06:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(name){
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+  static sanitize(name){
+    return name.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+  static titleize(name){
+  let arrPro=[]
+  let nameArr=name.split(" ");
+  nameArr.map(word =>{
+   if(word === 'the' || word ==='a' || word ==='an' || word ==='but' || word ==='of'|| word ==='and' || word ==='for' || word ==='at' || word ==='by' || word ==='from')
+   {
+     arrPro.push(word);
+   }
+   else{
+    arrPro.push(word.charAt(0).toUpperCase() + word.slice(1));
+   }
+   
+  
+  })
+  }
 }
